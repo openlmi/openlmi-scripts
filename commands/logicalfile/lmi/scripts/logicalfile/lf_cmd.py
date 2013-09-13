5e74b62283c256d5b0ef7b1256b7cc2f49b3b733
Jan Synacek <jsynacek@redhat.com>
2013-09-13 08:26:40 +0200

    add logicalfile scripts

---
+++ commands/logicalfile/lmi/scripts/logicalfile/lf_cmd.py
@@ -0,0 +1,113 @@
+# LogicalFile Management Providers
+#
+# Copyright (c) 2013, Red Hat, Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of the FreeBSD Project.
+#
+# Authors: Jan Synacek <jsynacek@redhat.com>
+#
+
+"""
+Logicalfile management functions.
+
+Usage:
+    %(cmd)s list <directory> [ <depth> ]
+    %(cmd)s createdir <directory>
+    %(cmd)s deletedir <directory>
+    %(cmd)s show <target>
+
+Commands:
+    list       List a directory. When depth is specified, at most depth levels
+               will be listed recursively.
+
+               The files and directories are listed in a tree-like structure.
+
+               Possible listed file types are:
+               F   : Regular data file.
+               Dev : Device file. Can be either block or character device.
+               Dir : Directory.
+               P   : Pipe file.
+               L   : Symbolic link.
+               S   : Unix socket.
+
+    createdir  Create a directory. The parent directory must exist.
+
+    deletedir  Delete a directory. The directory must be empty.
+
+    show       Show detailed information about target. Target can be any file
+               on the remote system.
+"""
+
+from lmi.scripts.common import command
+from lmi.scripts.logicalfile import logicalfile
+
+class Lister(command.LmiLister):
+    COLUMNS = ('Type', 'Name', 'Mode', 'Current SELinux Context')
+    OPT_NO_UNDERSCORES = True
+
+    def execute(self, ns, directory, depth=0):
+        """
+        Implementation of 'lf list' command.
+        """
+        return logicalfile.lf_list(ns, directory, depth)
+
+
+class Show(command.LmiLister):
+    COLUMNS = ('Name', 'Value')
+    OPT_NO_UNDERSCORES = True
+
+    def execute(self, ns, target):
+        """
+        Implementation of 'lf show' command.
+        """
+        return logicalfile.lf_show(ns, target)
+
+class CreateDir(command.LmiCheckResult):
+    EXPECT = None
+
+    def execute(self, ns, directory):
+        """
+        Implementation of 'lf createdir' command.
+        """
+        return logicalfile.lf_createdir(ns, directory)
+
+class DeleteDir(command.LmiCheckResult):
+    EXPECT = None
+
+    def execute(self, ns, directory):
+        """
+        Implementation of 'lf deletedir' command.
+        """
+        return logicalfile.lf_deletedir(ns, directory)
+
+Lf = command.register_subcommands(
+        'Lf', __doc__,
+        { 'list'    : Lister,
+          'createdir'  : CreateDir,
+          'deletedir'  : DeleteDir,
+          'show'    : Show,
+        },
+    )
---
+++ commands/logicalfile/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+PROJECT = 'openlmi-scripts-logicalfile'
+VERSION = '0.0.1'
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.md', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description='LMI command for system logical file administration.',
+    long_description=long_description,
+    author='Jan Synacek',
+    author_email='jsynacek@redhat.com',
+    url='https://github.com/openlmi/openlmi-scripts',
+    download_url='https://github.com/openlmi/openlmi-scripts/tarball/master',
+    platforms=['Any'],
+    license="BSD",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Systems Administration',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+    ],
+
+    install_requires=['openlmi-scripts', 'openlmi-tools'],
+
+    namespace_packages=['lmi', 'lmi.scripts' ],
+    packages=['lmi', 'lmi.scripts', 'lmi.scripts.logicalfile'],
+    include_package_data=True,
+
+    entry_points={
+        'lmi.scripts.cmd': [
+            'lf = lmi.scripts.logicalfile.lf_cmd:Lf',
+            ],
+        },
+    )
