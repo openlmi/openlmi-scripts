9aa8a65d2bce4377eda6803f6e220716e60d3def
Michal Minar <miminar@redhat.com>
2013-09-21 08:59:40 +0200

    do not render spaces at end of rows

--- lmi/scripts/common/formatter/__init__.py
+++ lmi/scripts/common/formatter/__init__.py
@@ -255,7 +255,9 @@ class TableFormatter(ListFormatter):
     def print_text_row(self, row, column_size):
         for i in xrange(len(row)):
             size = column_size[i]
-            item = self.render_value(unicode(row[i]).ljust(size))
+            item = self.render_value(row[i])
+            if i < len(row) - 1:
+                item = item.ljust(size)
             self.out.write(item)
             self.out.write(" ")
         self.out.write("\n")
