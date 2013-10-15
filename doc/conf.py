c14758735b13ff296ae5015b04865021c98f8d62
Michal Minar <miminar@redhat.com>
2013-10-15 12:41:37 +0200

    doc: build improvements
    Sphinx has difficulties in finding modules on given path.
    Added a script that installs all required python eggs in temporary
    directory and uses that for generating API documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../lmi'))
+sys.path.insert(0, os.path.abspath('..'))
 
 if os.environ.get('INCLUDE_COMMANDS', '0').lower() in {'1', 'true', 'yes'}:
     commands_path = os.environ.get('COMMANDS_PATH', '../commands')
