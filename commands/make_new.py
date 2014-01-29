fe1a6a5c34d2e84e0dbe0edf37e0ac4ad76566c9
Peter Schiffer <pschiffe@redhat.com>
2014-01-29 13:07:28 +0100

    Updated copyright year

--- commands/make_new.py
+++ commands/make_new.py
@@ -79,7 +79,7 @@ setup(
 """
 
 BSD_LICENSE_HEADER = \
-"""# Copyright (c) 2013, Red Hat, Inc. All rights reserved.
+"""# Copyright (C) 2013-2014 Red Hat, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
--- completion/lmi.bash
+++ completion/lmi.bash
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 Red Hat, Inc.  All rights reserved.
+# Copyright (C) 2013-2014 Red Hat, Inc.  All rights reserved.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
