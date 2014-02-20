f5865af01d31a808cce53901ab7d7674ca982863
Radek Novacek <rnovacek@redhat.com>
2014-02-20 15:58:13 +0100

    networking: add myself as an author to the modules

--- commands/networking/lmi/scripts/networking/util.py
+++ commands/networking/lmi/scripts/networking/util.py
@@ -24,7 +24,9 @@
 # The views and conclusions contained in the software and documentation are
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of the FreeBSD Project.
-
+#
+# Authors: Radek Novacek <rnovacek@redhat.com>
+#
 """
 LMI networking script utilities library.
 """
